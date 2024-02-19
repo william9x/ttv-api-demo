7edcfe76a8c2464402debb1f79c60c25ef28b034
LiamH <lehoangnam197@gmail.com>
2024-02-19 10:44:59 +0700

    Init

---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+from datetime import datetime
+
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+
+from inferer_cerspense import CerspenseInferer
+
+app = FastAPI()
+
+
+class InferReq(BaseModel):
+    prompt: str | None
+    num_inference_steps: int = 25
+    num_upscale_steps: int = 25
+    height: int = 576
+    width: int = 1024
+    upscale: bool = False
+    upscaled_height: int = 576
+    upscaled_width: int = 1024
+    num_frames: int = 30
+    strength: float = 0.6
+    negative_prompt: str | None
+    guidance_scale: float = 10.0
+
+
+@app.post("/infer/cerspense", tags=["Infer"], response_class=FileResponse)
+def infer(req: InferReq):
+    now = datetime.now().strftime("%m%d_%H%M%S")
+    output_path = f"{os.getcwd()}/output/{now}.mp4"
+    video_path = CerspenseInferer().generate_video(
+        prompt=req.prompt,
+        num_inference_steps=req.num_inference_steps,
+        num_upscale_steps=req.num_upscale_steps,
+        height=req.height,
+        width=req.width,
+        upscale=req.upscale,
+        upscaled_height=req.upscaled_height,
+        upscaled_width=req.upscaled_width,
+        num_frames=req.num_frames,
+        strength=req.strength,
+        output_path=output_path,
+        negative_prompt=req.negative_prompt,
+        guidance_scale=req.guidance_scale,
+    )
+    return FileResponse(
+        path=video_path,
+        status_code=201,
+        media_type="application/octet-stream"
+    )
