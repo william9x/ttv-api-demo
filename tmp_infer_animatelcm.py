51e087fc9a733bc9825dccac480634dff5b34529
LiamH <lehoangnam197@gmail.com>
2024-02-22 16:42:07 +0700

    tmp: Add tmp

---
+++ tmp_infer_animatelcm.py
@@ -0,0 +1,69 @@
+import torch
+from diffusers import AnimateDiffPipeline, MotionAdapter, LCMScheduler
+from diffusers.utils import export_to_video
+from diffusers.utils.import_utils import is_xformers_available
+
+
+class AnimateLCMInfer:
+
+    # Function to initialize the AnimateDiffPipeline
+    def initialize_animate_diff_pipeline(self, dtype=torch.float16, chunk_size=1, dim=1):
+        adapter = MotionAdapter.from_pretrained("wangfuyun/AnimateLCM", torch_dtype=dtype)
+        pipe = AnimateDiffPipeline.from_pretrained(
+            "emilianJR/epiCRealism",
+            motion_adapter=adapter,
+            torch_dtype=dtype
+        )
+        pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config, beta_schedule="linear")
+
+        pipe.load_lora_weights(
+            "wangfuyun/AnimateLCM",
+            weight_name="sd15_lora_beta.safetensors",
+            adapter_name="lcm-lora"
+        )
+        pipe.set_adapters(["lcm-lora"], [0.8])
+
+        # pipe.unet.enable_forward_chunking(chunk_size=chunk_size, dim=dim)
+
+        pipe.enable_vae_slicing()
+
+        if is_xformers_available():
+            pipe.enable_xformers_memory_efficient_attention()
+
+        pipe.enable_model_cpu_offload()
+        return pipe
+
+    # Function to export frames to a video
+    def export_frames_to_video(self, frames, output_path):
+        video_path = export_to_video(frames, output_video_path=output_path)
+        print(f"Video generated: {video_path}")
+        return video_path
+
+    # Main function to generate videos
+    def generate_video(self, prompt=None, num_inference_steps=None, height=None, width=None,
+                       num_frames=None, output_path=None, negative_prompt=None, guidance_scale=None, strength=None):
+        prompt = prompt.strip() if prompt is not None else "Space scenery"
+        num_inference_steps = int(num_inference_steps) if num_inference_steps is not None else 30
+        height = int(height) if height is not None else 576
+        width = int(width) if width is not None else 1024
+        num_frames = int(num_frames) if num_frames is not None else 30
+        negative_prompt = negative_prompt.strip() if negative_prompt is not None else None  # Convert to a single string
+        guidance_scale = float(guidance_scale) if guidance_scale is not None else 10.0
+        output_path = output_path or "./output/"
+
+        # Create the pipeline once outside the loop
+        pipe = self.initialize_animate_diff_pipeline()
+
+        # Generate video frames
+        video_frames = pipe(
+            prompt=prompt,
+            num_inference_steps=num_inference_steps,
+            height=height,
+            width=width,
+            num_frames=num_frames,
+            negative_prompt=negative_prompt,
+            guidance_scale=guidance_scale,
+            generator=torch.Generator(device='cuda'),
+        ).frames
+
+        return self.export_frames_to_video(video_frames[0], output_path)
---
+++ tmp_main.py
@@ -0,0 +1,120 @@
+import os
+from datetime import datetime
+
+from fastapi import FastAPI
+from fastapi.responses import FileResponse, JSONResponse
+from pydantic import BaseModel
+
+from infer_animatelcm import AnimateLCMInfer
+from infer_zeroscope import ZeroScopeInfer
+
+app = FastAPI()
+
+lock = False
+
+
+class ZeroScopeInferReq(BaseModel):
+    prompt: str | None
+    num_inference_steps: int = 25
+    num_upscale_steps: int = 25
+    height: int = 576
+    width: int = 1024
+    upscale: bool = False
+    upscaled_height: int = 576
+    upscaled_width: int = 1024
+    num_frames: int = 30
+    strength: float = 0.6
+    negative_prompt: str | None
+    guidance_scale: float = 10.0
+
+
+@app.post("/infer/zeroscope", tags=["Infer"], response_class=FileResponse)
+def infer(req: ZeroScopeInferReq):
+    global lock
+    if lock:
+        return JSONResponse(content={"message": "Server is busy"}, status_code=503)
+    lock = True
+
+    now = datetime.now().strftime("%m%d_%H%M%S")
+    output_path = f"{os.getcwd()}/output/{now}.mp4"
+    video_path = ZeroScopeInfer().generate_video(
+        prompt=req.prompt,
+        num_inference_steps=req.num_inference_steps,
+        num_upscale_steps=req.num_upscale_steps,
+        height=req.height,
+        width=req.width,
+        upscale=req.upscale,
+        upscaled_height=req.upscaled_height,
+        upscaled_width=req.upscaled_width,
+        num_frames=req.num_frames,
+        strength=req.strength,
+        output_path=output_path,
+        negative_prompt=req.negative_prompt,
+        guidance_scale=req.guidance_scale,
+    )
+
+    lock = False
+    return FileResponse(
+        path=video_path,
+        status_code=201,
+        media_type="application/octet-stream",
+        filename=f"{now}.mp4",
+    )
+
+
+class AnimateLCMInferReq(BaseModel):
+    prompt: str | None
+    negative_prompt: str | None
+    num_inference_steps: int = 25
+    num_frames: int = 16
+    width: int = 512
+    height: int = 512
+    guidance_scale: float = 2.0
+    output_file_path: str | None
+
+
+@app.post("/api/v1/infer/animate_lcm", tags=["Infer"], response_class=JSONResponse)
+def infer(req: AnimateLCMInferReq):
+    # global lock
+    # if lock:
+    #     return JSONResponse(content={"message": "Server is busy"}, status_code=503)
+    # lock = True
+
+    # if req.num_inference_steps > 25:
+    #     return JSONResponse(content={"message": "maximum num_inference_steps is 25"}, status_code=400)
+    #
+    # if req.num_frames > 20:
+    #     return JSONResponse(content={"message": "maximum num_frames is 20"}, status_code=400)
+    #
+    # if req.guidance_scale > 2:
+    #     return JSONResponse(content={"message": "maximum guidance_scale is 2.0"}, status_code=400)
+
+    if req.output_file_path is None:
+        return JSONResponse(content={"message": "missing output path"}, status_code=400)
+
+    print(req)
+    print(f"OUTPUT PATH: {req.output_file_path}")
+    try:
+        video_path = AnimateLCMInfer().generate_video(
+            prompt=req.prompt,
+            num_inference_steps=req.num_inference_steps,
+            height=req.height,
+            width=req.width,
+            num_frames=req.num_frames,
+            negative_prompt=req.negative_prompt,
+            guidance_scale=req.guidance_scale,
+            output_path=req.output_file_path,
+        )
+    except Exception as e:
+        # lock = False
+        print(e)
+        return JSONResponse(content={"message": "Internal Server Error"}, status_code=500)
+
+    # lock = False
+    return JSONResponse(content={"message": "Created"}, status_code=201)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8080)
