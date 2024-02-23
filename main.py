64057a9442a1725b53b6151eab15e022f66659fe
LiamH <lehoangnam197@gmail.com>
2024-02-23 15:25:44 +0700

    update

--- main.py
+++ main.py
@@ -1,68 +1,12 @@
 import os
-from datetime import datetime
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse, JSONResponse
 from pydantic import BaseModel
 
 from infer_animatelcm import AnimateLCMInfer
-from infer_zeroscope import ZeroScopeInfer
 
 app = FastAPI()
-
-lock = False
-counter = 1
-counter_2 = 1
-
-
-class ZeroScopeInferReq(BaseModel):
-    prompt: str | None
-    num_inference_steps: int = 25
-    num_upscale_steps: int = 25
-    height: int = 576
-    width: int = 1024
-    upscale: bool = False
-    upscaled_height: int = 576
-    upscaled_width: int = 1024
-    num_frames: int = 30
-    strength: float = 0.6
-    negative_prompt: str | None
-    guidance_scale: float = 10.0
-
-
-@app.post("/infer/zeroscope", tags=["Infer"], response_class=FileResponse)
-def infer(req: ZeroScopeInferReq):
-    # global lock
-    # if lock:
-    #     return JSONResponse(content={"message": "Server is busy"}, status_code=503)
-    # lock = True
-    global counter
-
-    now = datetime.now().strftime("%m%d_%H%M%S")
-    output_path = f"{os.getcwd()}/output/{now}.mp4"
-    video_path = ZeroScopeInfer().generate_video(
-        prompt=req.prompt,
-        num_inference_steps=req.num_inference_steps,
-        num_upscale_steps=req.num_upscale_steps,
-        height=req.height,
-        width=req.width,
-        upscale=req.upscale,
-        upscaled_height=req.upscaled_height,
-        upscaled_width=req.upscaled_width,
-        num_frames=req.num_frames,
-        strength=req.strength,
-        output_path=output_path,
-        negative_prompt=req.negative_prompt,
-        guidance_scale=req.guidance_scale,
-    )
-
-    # lock = False
-    return FileResponse(
-        path=video_path,
-        status_code=201,
-        media_type="application/octet-stream",
-        filename=f"{now}.mp4",
-    )
 
 
 class AnimateLCMInferReq(BaseModel):
@@ -77,8 +21,6 @@ class AnimateLCMInferReq(BaseModel):
 
 @app.post("/infer/animate_lcm", tags=["Infer"], response_class=FileResponse)
 def infer(req: AnimateLCMInferReq):
-    global counter
-
     if req.num_inference_steps > 25:
         return JSONResponse(content={"message": "maximum num_inference_steps is 25"}, status_code=400)
 
@@ -88,7 +30,7 @@ def infer(req: AnimateLCMInferReq):
     if req.guidance_scale > 2:
         return JSONResponse(content={"message": "maximum guidance_scale is 2.0"}, status_code=400)
 
-    output_path = f"{os.getcwd()}/output/animate_lcm_{counter_2}.mp4"
+    output_path = f"{os.getcwd()}/output/animate_lcm.mp4"
     try:
         video_path = AnimateLCMInfer().generate_video(
             prompt=req.prompt,
@@ -104,12 +46,11 @@ def infer(req: AnimateLCMInferReq):
         print(e)
         return JSONResponse(content={"message": "Internal Server Error"}, status_code=500)
 
-    counter_2 += 1
     return FileResponse(
         path=video_path,
         status_code=201,
         media_type="application/octet-stream",
-        filename=f"animate_lcm_{counter_2}.mp4",
+        filename=f"animate_lcm.mp4",
     )
 
 
