aa3cc664d09785d2c522c8ffe3f82b7929c3953b
LiamH <lehoangnam197@gmail.com>
2024-02-27 10:47:05 +0700

    update

--- infer_animatelcm.py
+++ infer_animatelcm.py
@@ -74,39 +74,6 @@ class AnimateLCMInfer:
             generator=torch.Generator(device='cuda'),
         ).frames
 
+        del pipe
+
         return self.export_frames_to_video(video_frames[0], output_path)
-
-    def encode_prompt(selft, prompts, tokenizers, text_encoders):
-        embeddings_list = []
-
-        for prompt, tokenizer, text_encoder in zip(prompts, tokenizers, text_encoders):
-            cond_input = tokenizer(
-                prompt,
-                max_length=tokenizer.model_max_length,
-                padding='max_length',
-                truncation=True,
-                return_tensors='pt',
-            )
-
-            prompt_embeds = text_encoder(cond_input.input_ids.to('cuda'), output_hidden_states=True)
-
-            pooled_prompt_embeds = prompt_embeds[0]
-            embeddings_list.append(prompt_embeds.hidden_states[-2])
-
-        prompt_embeds = torch.concat(embeddings_list, dim=-1)
-
-        negative_prompt_embeds = torch.zeros_like(prompt_embeds)
-        negative_pooled_prompt_embeds = torch.zeros_like(pooled_prompt_embeds)
-
-        bs_embed, seq_len, _ = prompt_embeds.shape
-        prompt_embeds = prompt_embeds.repeat(1, 1, 1)
-        prompt_embeds = prompt_embeds.view(bs_embed * 1, seq_len, -1)
-
-        seq_len = negative_prompt_embeds.shape[1]
-        negative_prompt_embeds = negative_prompt_embeds.repeat(1, 1, 1)
-        negative_prompt_embeds = negative_prompt_embeds.view(1 * 1, seq_len, -1)
-
-        pooled_prompt_embeds = pooled_prompt_embeds.repeat(1, 1).view(bs_embed * 1, -1)
-        negative_pooled_prompt_embeds = negative_pooled_prompt_embeds.repeat(1, 1).view(bs_embed * 1, -1)
-
-        return prompt_embeds, negative_prompt_embeds, pooled_prompt_embeds, negative_pooled_prompt_embeds
