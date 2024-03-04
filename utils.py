9e224acfe13f4863d02fe0d636f58326ff2979e9
LiamH <lehoangnam197@gmail.com>
2024-03-04 16:10:44 +0700

    feat: Use local utils export video

--- utils.py
+++ utils.py
@@ -1,10 +1,40 @@
-import torch
+import tempfile
+from typing import List, Union
 
-from diffusers.utils import export_to_video
+import PIL.Image
+import PIL.ImageOps
+import numpy as np
+import torch
+from diffusers.utils.import_utils import is_opencv_available, BACKENDS_MAPPING
+
+
+def _export_to_video(
+        video_frames: Union[List[np.ndarray], List[PIL.Image.Image]], output_video_path: str = None, fps: int = 8
+) -> str:
+    if is_opencv_available():
+        import cv2
+    else:
+        raise ImportError(BACKENDS_MAPPING["opencv"][1].format("export_to_video"))
+    if output_video_path is None:
+        output_video_path = tempfile.NamedTemporaryFile(suffix=".mp4").name
+
+    if isinstance(video_frames[0], np.ndarray):
+        video_frames = [(frame * 255).astype(np.uint8) for frame in video_frames]
+
+    elif isinstance(video_frames[0], PIL.Image.Image):
+        video_frames = [np.array(frame) for frame in video_frames]
+
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    h, w, c = video_frames[0].shape
+    video_writer = cv2.VideoWriter(output_video_path, fourcc, fps=fps, frameSize=(w, h))
+    for i in range(len(video_frames)):
+        img = cv2.cvtColor(video_frames[i], cv2.COLOR_RGB2BGR)
+        video_writer.write(img)
+    return output_video_path
 
 
 def export_frames_to_video(frames, output_path):
-    video_path = export_to_video(frames, output_video_path=output_path)
+    video_path = _export_to_video(frames, output_video_path=output_path)
     print(f"Video generated: {video_path}")
     return video_path
 
