99e8b2f329fb82837988d9fb9a44e95b4cdcb84e
LiamH <lehoangnam197@gmail.com>
2024-03-07 14:14:40 +0700

    embed prompts

--- utils.py
+++ utils.py
@@ -30,13 +30,14 @@ def generate_video(
         guidance_scale=1.5,
 ):
     # Generate video frames
+    prompt_embeds, negative_prompt_embeds = get_pipeline_embeds(pipe, prompt, negative_prompt, "cuda")
     video_frames = pipe(
-        prompt=prompt,
+        prompt_embeds=prompt_embeds,
+        negative_prompt_embeds=negative_prompt_embeds,
         num_inference_steps=num_inference_steps,
         height=height,
         width=width,
         num_frames=num_frames,
-        negative_prompt=negative_prompt,
         guidance_scale=guidance_scale,
         generator=torch.Generator(),
     ).frames
@@ -44,3 +45,37 @@ def generate_video(
     torch.cuda.empty_cache()
 
     return export_frames_to_video(video_frames[0], output_path)
+
+
+def get_pipeline_embeds(pipeline, prompt, negative_prompt, device):
+    """ Get pipeline embeds for prompts bigger than the maxlength of the pipe
+    :param pipeline:
+    :param prompt:
+    :param negative_prompt:
+    :param device:
+    :return:
+    """
+    max_length = pipeline.tokenizer.model_max_length
+
+    # simple way to determine length of tokens
+    input_ids = pipeline.tokenizer(prompt, return_tensors="pt", truncation=False).input_ids.to(device)
+    negative_ids = pipeline.tokenizer(negative_prompt, return_tensors="pt", truncation=False).input_ids.to(device)
+
+    # create the tensor based on which prompt is longer
+    if input_ids.shape[-1] >= negative_ids.shape[-1]:
+        shape_max_length = input_ids.shape[-1]
+        negative_ids = pipeline.tokenizer(negative_prompt, truncation=False, padding="max_length",
+                                          max_length=shape_max_length, return_tensors="pt").input_ids.to(device)
+
+    else:
+        shape_max_length = negative_ids.shape[-1]
+        input_ids = pipeline.tokenizer(prompt, return_tensors="pt", truncation=False, padding="max_length",
+                                       max_length=shape_max_length).input_ids.to(device)
+
+    concat_embeds = []
+    neg_embeds = []
+    for i in range(0, shape_max_length, max_length):
+        concat_embeds.append(pipeline.text_encoder(input_ids[:, i: i + max_length])[0])
+        neg_embeds.append(pipeline.text_encoder(negative_ids[:, i: i + max_length])[0])
+
+    return torch.cat(concat_embeds, dim=1), torch.cat(neg_embeds, dim=1)
