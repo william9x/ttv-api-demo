08bfd86360ea88e61713616a1c36b0ae38ba4230
LiamH <lehoangnam197@gmail.com>
2024-03-07 14:45:36 +0700

    update compel

--- utils.py
+++ utils.py
@@ -31,7 +31,7 @@ def generate_video(
         guidance_scale=1.5,
 ):
     # Generate video frames
-    compel = Compel(tokenizer=pipe.tokenizer, text_encoder=pipe.text_encoder, truncate_long_prompts=False)
+    compel = Compel(tokenizer=pipe.tokenizer, text_encoder=pipe.text_encoder, truncate_long_prompts=False, device="cuda")
     conditioning = compel.build_conditioning_tensor(prompt)
     neg_conditioning = compel.build_conditioning_tensor(negative_prompt)
     [conditioning, neg_conditioning] = compel.pad_conditioning_tensors_to_same_length([conditioning, neg_conditioning])
