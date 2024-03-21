c6402cc367aacfe442c592ae552f489277047a4c
LiamH <lehoangnam197@gmail.com>
2024-03-21 16:33:07 +0700

    disable xformers_memory_efficient_attention

--- animate_lcm_factory.py
+++ animate_lcm_factory.py
@@ -42,8 +42,8 @@ class AnimateDiffFactory:
 
         # Must be in order
         print(f"[AnimateDiffFactory] Optimizing model {model_path}")
-        # pipe.enable_vae_slicing()
-        pipe.enable_xformers_memory_efficient_attention()
+        pipe.enable_vae_slicing()
+        # pipe.enable_xformers_memory_efficient_attention()
         pipe.enable_model_cpu_offload()
         # pipe.to("cuda")
         # tomesd.apply_patch(pipe, ratio=0.5)
