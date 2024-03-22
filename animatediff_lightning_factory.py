e382a5aeaa063ad3192f7d0845d39c29790fba92
LiamH <lehoangnam197@gmail.com>
2024-03-22 17:26:54 +0700

    feat: test LCMScheduler

--- animatediff_lightning_factory.py
+++ animatediff_lightning_factory.py
@@ -34,7 +34,7 @@ class AnimateDiffLightningFactory:
             torch_dtype=self.dtype,
             motion_adapter=adapter,
         ).to(self.device)
-        pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config, beta_schedule="linear")
+        pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing", beta_schedule="linear")
         # pipe.scheduler = EulerDiscreteScheduler.from_config(
         #     pipe.scheduler.config,
         #     timestep_spacing="trailing",
